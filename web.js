9026e6f8fa97aca6a27cb99da59b153b4427645d
Rizwan Khan <rizkhan@gmail.com>
2013-07-05 12:16:23 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var string= fs.readFileSync('index.html', 'utf8')
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
